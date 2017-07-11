a88c9f9378b2927bb89dd37f59d30ea46cba3d47
Kustiawanto Halim <kustiawanto.halim@gmail.com>
2017-07-11 22:25:01 +0700

    add 404

--- server.js
+++ server.js
@@ -3,15 +3,12 @@
 require('dotenv').config();
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 3000;
+var port = process.env.PORT;
 var mongoose = require('mongoose');
 var Promise = require('bluebird');
 var bodyParser = require('body-parser');
 var User = require('./api/models/User');
-
-var uristring =
-    process.env.MONGODB_URI ||
-    'mongodb://localhost/pasarku';
+var uristring = process.env.MONGODB_URI;
 
 mongoose.Promise = Promise;
 mongoose.connect(uristring, function(err, res) {
@@ -26,9 +23,14 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+app.set('view engine', 'pug');
 
 var routes = require('./api/routes/routes');
 routes(app);
+
+app.get('*', (req, res) => {
+    res.render('404');
+});
 
 app.listen(port);
 
